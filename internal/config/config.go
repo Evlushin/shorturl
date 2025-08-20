c325347b5945903da64c8078da54354b7f40c83b
aevlushin <aevlushin@atsaero.ru>
2025-08-20 11:07:02 +0300

    Инкремент 4

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,8 @@ type Config struct {
 
 func GetConfig() Config {
 	cfg := Config{}
-	flag.StringVar(&cfg.Handlers.ServerAddr, "addr", "localhost:8080", "address of HTTP server")
+	flag.StringVar(&cfg.Handlers.ServerAddr, "a", "localhost:8080", "address of HTTP server")
+	flag.StringVar(&cfg.Handlers.BaseAddr, "b", "localhost:8080", "base address of the resulting shortened URL")
 
 	flag.Parse()
 	return cfg
