bfbc11f6a90cce4396719ce48ed9337c0c66a4ca
aevlushin <aevlushin@atsaero.ru>
2025-08-26 10:37:56 +0300

    Инкремент 5

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	handlersConfig "github.com/Evlushin/shorturl/internal/handler/config"
+	"os"
 )
 
 type Config struct {
@@ -11,9 +12,18 @@ type Config struct {
 
 func GetConfig() Config {
 	cfg := Config{}
+
 	flag.StringVar(&cfg.Handlers.ServerAddr, "a", "localhost:8080", "address of HTTP server")
 	flag.StringVar(&cfg.Handlers.BaseAddr, "b", "http://localhost:8080", "base address of the resulting shortened URL")
-
 	flag.Parse()
+
+	if serverAddr := os.Getenv("SERVER_ADDRESS"); serverAddr != "" {
+		cfg.Handlers.ServerAddr = serverAddr
+	}
+
+	if baseAddr := os.Getenv("BASE_URL"); baseAddr != "" {
+		cfg.Handlers.BaseAddr = baseAddr
+	}
+
 	return cfg
 }
