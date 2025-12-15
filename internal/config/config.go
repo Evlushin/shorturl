b7d9bd53c9a8c4a02df7190e84002b786b5d2117
aevlushin <aevlushin@atsaero.ru>
2025-12-15 19:27:09 +0300

    Инкремент 23

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,9 +8,9 @@ import (
 
 type Config struct {
 	Handlers      handlersConfig.Config
-	LogLevel      string `env:"LOG_LEVEL" flag:"l" default:"info" help:"log level"`
-	FileStorePath string `env:"FILE_STORAGE_PATH" flag:"f" default:"" help:"address storage"`
-	DatabaseDsn   string `env:"DATABASE_DSN" flag:"d" default:"" help:"connection string"`
+	LogLevel      string `env:"LOG_LEVEL" flag:"l" env-default:"info" env-description:"log level"`
+	FileStorePath string `env:"FILE_STORAGE_PATH" flag:"f" env-default:"" env-description:"address storage"`
+	DatabaseDsn   string `env:"DATABASE_DSN" flag:"d" env-default:"" env-description:"connection string"`
 }
 
 func GetConfig() (Config, error) {
--- internal/handler/config/config.go
+++ internal/handler/config/config.go
@@ -3,16 +3,16 @@ package config
 import "time"
 
 type TAudit struct {
-	AuditFile string `env:"AUDIT_FILE" flag:"audit-file" default:"audit.txt" help:"path to the destination file where audit logs are saved"`
-	AuditURL  string `env:"AUDIT_URL" flag:"audit-url" default:"" help:"the full URL of the remote receiving server where the audit logs are sent"`
+	AuditFile string `env:"AUDIT_FILE" flag:"audit-file" env-default:"audit.txt" env-description:"path to the destination file where audit logs are saved"`
+	AuditURL  string `env:"AUDIT_URL" flag:"audit-url" env-default:"" env-description:"the full URL of the remote receiving server where the audit logs are sent"`
 }
 
 type Config struct {
-	ServerAddr   string `env:"SERVER_ADDRESS" flag:"a" default:"localhost:8080" help:"address of HTTP server"`
-	BaseAddr     string `env:"BASE_URL" flag:"b" default:"http://localhost:8080" help:"base address of the resulting shortened URL"`
-	SecretKey    string `env:"SECRET_KEY" flag:"s" default:"{{uuid}}" help:"secret key"`
+	ServerAddr   string `env:"SERVER_ADDRESS" flag:"a" env-default:"localhost:8080" env-description:"address of HTTP server"`
+	BaseAddr     string `env:"BASE_URL" flag:"b" env-default:"http://localhost:8080" env-description:"base address of the resulting shortened URL"`
+	SecretKey    string `env:"SECRET_KEY" flag:"s" env-default:"{{uuid}}" env-description:"secret key"`
 	Audit        TAudit
-	ReadTimeout  time.Duration `env:"READ_TIMEOUT" default:"5s" help:"HTTP server read timeout"`
-	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" default:"10s" help:"HTTP server write timeout"`
-	IdleTimeout  time.Duration `env:"IDLE_TIMEOUT" default:"120s" help:"HTTP server idle timeout"`
+	ReadTimeout  time.Duration `env:"READ_TIMEOUT" env-default:"5s" env-description:"HTTP server read timeout"`
+	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" env-default:"10s" env-description:"HTTP server write timeout"`
+	IdleTimeout  time.Duration `env:"IDLE_TIMEOUT" env-default:"120s" env-description:"HTTP server idle timeout"`
 }
