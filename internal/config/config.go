58465a39fbe41e3982a029324453ab7b1371706b
aevlushin <aevlushin@atsaero.ru>
2025-12-15 19:52:42 +0300

    Инкремент 23

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"flag"
+	"github.com/google/uuid"
 	"github.com/ilyakaznacheev/cleanenv"
 
 	handlersConfig "github.com/Evlushin/shorturl/internal/handler/config"
@@ -8,13 +10,25 @@ import (
 
 type Config struct {
 	Handlers      handlersConfig.Config
-	LogLevel      string `env:"LOG_LEVEL" flag:"l" env-default:"info" env-description:"log level"`
-	FileStorePath string `env:"FILE_STORAGE_PATH" flag:"f" env-default:"" env-description:"address storage"`
-	DatabaseDsn   string `env:"DATABASE_DSN" flag:"d" env-default:"" env-description:"connection string"`
+	LogLevel      string `env:"LOG_LEVEL" env-default:"info" env-description:"log level"`
+	FileStorePath string `env:"FILE_STORAGE_PATH" env-default:"" env-description:"address storage"`
+	DatabaseDsn   string `env:"DATABASE_DSN" env-default:"" env-description:"connection string"`
 }
 
 func GetConfig() (Config, error) {
 	var cfg Config
+
+	flag.StringVar(&cfg.Handlers.Audit.AuditFile, "audit-file", "audit.txt", "path to the destination file where audit logs are saved")
+	flag.StringVar(&cfg.Handlers.Audit.AuditURL, "audit-url", "", "the full URL of the remote receiving server where the audit logs are sent")
+	flag.StringVar(&cfg.Handlers.ServerAddr, "a", "localhost:8080", "address of HTTP server")
+	flag.StringVar(&cfg.Handlers.BaseAddr, "b", "http://localhost:8080", "base address of the resulting shortened URL")
+	flag.StringVar(&cfg.LogLevel, "l", "info", "log level")
+	//flag.StringVar(&cfg.FileStorePath, "f", "storage.txt", "address storage")
+	//flag.StringVar(&cfg.DatabaseDsn, "d", "host=127.127.126.41 port=5432 dbname=shorturl user=shorturl password=shorturl connect_timeout=10 sslmode=prefer", "connection string")
+	flag.StringVar(&cfg.FileStorePath, "f", "", "address storage")
+	flag.StringVar(&cfg.DatabaseDsn, "d", "", "connection string")
+	flag.StringVar(&cfg.Handlers.SecretKey, "s", uuid.NewString(), "secret key")
+	flag.Parse()
 
 	if err := cleanenv.ReadConfig(".env", &cfg); err != nil {
 		return Config{}, err
--- internal/handler/config/config.go
+++ internal/handler/config/config.go
@@ -3,14 +3,14 @@ package config
 import "time"
 
 type TAudit struct {
-	AuditFile string `env:"AUDIT_FILE" flag:"audit-file" env-default:"audit.txt" env-description:"path to the destination file where audit logs are saved"`
-	AuditURL  string `env:"AUDIT_URL" flag:"audit-url" env-default:"" env-description:"the full URL of the remote receiving server where the audit logs are sent"`
+	AuditFile string `env:"AUDIT_FILE" env-default:"audit.txt" env-description:"path to the destination file where audit logs are saved"`
+	AuditURL  string `env:"AUDIT_URL" env-default:"" env-description:"the full URL of the remote receiving server where the audit logs are sent"`
 }
 
 type Config struct {
-	ServerAddr   string `env:"SERVER_ADDRESS" flag:"a" env-default:"localhost:8080" env-description:"address of HTTP server"`
-	BaseAddr     string `env:"BASE_URL" flag:"b" env-default:"http://localhost:8080" env-description:"base address of the resulting shortened URL"`
-	SecretKey    string `env:"SECRET_KEY" flag:"s" env-default:"{{uuid}}" env-description:"secret key"`
+	ServerAddr   string `env:"SERVER_ADDRESS" env-default:"localhost:8080" env-description:"address of HTTP server"`
+	BaseAddr     string `env:"BASE_URL" env-default:"http://localhost:8080" env-description:"base address of the resulting shortened URL"`
+	SecretKey    string `env:"SECRET_KEY" env-default:"{{uuid}}" env-description:"secret key"`
 	Audit        TAudit
 	ReadTimeout  time.Duration `env:"READ_TIMEOUT" env-default:"5s" env-description:"HTTP server read timeout"`
 	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" env-default:"10s" env-description:"HTTP server write timeout"`
